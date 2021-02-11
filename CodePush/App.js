aaa260c021388d5dfcc97cc1627acf4ed91d5df3
Zulfian <zulfian.nutech@gmail.com>
2021-02-11 11:54:18 +0700

    up

--- CodePush/App.js
+++ CodePush/App.js
@@ -3,7 +3,7 @@ import {View, Text, StatusBar} from 'react-native';
 import codePush from 'react-native-code-push';
 
 const App = () => {
-  const env = 'prod';
+  const env = 'dev';
   const whatEnv = env === 'dev' ? '__DEV__' : '__PROD__';
 
   return (
@@ -25,14 +25,7 @@ const App = () => {
   );
 };
 
-// export default App;
-
-/**
- * if in console, 400: An update check must include a valid deployment key - please check that your app has been configured correctly. To view available deployment keys, run 'code-push deployment ls <appName> -k'.
- * active code below
- */
-
 export default codePush({
   checkFrequency: codePush.CheckFrequency.ON_APP_RESUME,
-  installMode: codePush.InstallMode.ON_NEXT_RESUME,
+  installMode: codePush.InstallMode.IMMEDIATE,
 })(App);
